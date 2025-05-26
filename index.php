d3141020056dc91813e197d5782f5b8fd174826d
Maya Tunde Olawale <mayadihno@gmail.com>
2025-05-26 08:43:03 +0100

    `Removed commented out configuration settings and unused JavaScript code.`

--- index.php
+++ index.php
@@ -2,4 +2,3 @@
 session_start();
 require './private/core/autoload.php';
 $app = new App();
-echo '<script>var ROOT = "' . ROOT . '";</script>';
--- private/core/config.php
+++ private/core/config.php
@@ -1,23 +1,23 @@
 <?php
 
-// define('ROOT', 'scholatical.infinityfreeapp.com/');
-// define('ASSETS', 'scholatical.infinityfreeapp.com/assets/');
+define('ROOT', 'scholatical.infinityfreeapp.com/');
+define('ASSETS', 'scholatical.infinityfreeapp.com/assets/');
 
 
-// define('DB_HOST', 'sql305.infinityfree.com');
-// define('DB_USER', 'if0_39075847');
-// define('DB_PASS', 'poB1nRF4uKM');
-// define('DB_NAME', 'if0_39075847_school_db');
-// define('DB_DRIVER', 'mysql');
+define('DB_HOST', 'sql305.infinityfree.com');
+define('DB_USER', 'if0_39075847');
+define('DB_PASS', 'poB1nRF4uKM');
+define('DB_NAME', 'if0_39075847_school_db');
+define('DB_DRIVER', 'mysql');
 
 
 define('RANDOM', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
 
 
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'school_db');
-define('DB_DRIVER', 'mysql');
-define('DB_HOST', 'localhost');
-define('ROOT', 'http://localhost/school_management/');
-define('ASSETS', 'http://localhost/school_management/assets/');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'school_db');
+// define('DB_DRIVER', 'mysql');
+// define('DB_HOST', 'localhost');
+// define('ROOT', 'http://localhost/school_management/');
+// define('ASSETS', 'http://localhost/school_management/assets/');
