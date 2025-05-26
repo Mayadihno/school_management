070c80dde7ba5b5fe8c5ba0b76e061763b6d77ab
Maya Tunde Olawale <mayadihno@gmail.com>
2025-05-26 08:17:04 +0100

    refactor the folder structure

--- private/views/includes/header.view.php
+++ private/views/includes/header.view.php
@@ -7,6 +7,7 @@
     <title>Home</title>
     <link rel="stylesheet" type="text/css" href="<?= ROOT; ?>assets/bootstrap.min.css">
     <link rel="stylesheet" type="text/css" href="<?= ROOT; ?>assets/all.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <style>
         a {
