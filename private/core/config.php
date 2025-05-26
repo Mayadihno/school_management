0cae79b4670981efb643d9c4eda0102da0e12525
Maya Tunde Olawale <mayadihno@gmail.com>
2025-05-26 19:15:03 +0100

    Uncomment database and path definitions in config.php for active configuration

--- private/core/config.php
+++ private/core/config.php
@@ -1,24 +1,24 @@
 <?php
 
-// define('ROOT', 'https://scholatical.infinityfreeapp.com/');
-// define('ASSETS', 'https://scholatical.infinityfreeapp.com/assets/');
+define('ROOT', 'https://scholatical.infinityfreeapp.com/');
+define('ASSETS', 'https://scholatical.infinityfreeapp.com/assets/');
 
 
 
-// define('DB_HOST', 'sql305.infinityfree.com');
-// define('DB_USER', 'if0_39075847');
-// define('DB_PASS', 'poB1nRF4uKM');
-// define('DB_NAME', 'if0_39075847_school_db');
-// define('DB_DRIVER', 'mysql');
+define('DB_HOST', 'sql305.infinityfree.com');
+define('DB_USER', 'if0_39075847');
+define('DB_PASS', 'poB1nRF4uKM');
+define('DB_NAME', 'if0_39075847_school_db');
+define('DB_DRIVER', 'mysql');
 
 
 define('RANDOM', 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789');
 
 
-define('DB_USER', 'root');
-define('DB_PASS', '');
-define('DB_NAME', 'school_db');
-define('DB_DRIVER', 'mysql');
-define('DB_HOST', 'localhost');
-define('ROOT', 'http://localhost/school_management/');
-define('ASSETS', 'http://localhost/school_management/assets/');
+// define('DB_USER', 'root');
+// define('DB_PASS', '');
+// define('DB_NAME', 'school_db');
+// define('DB_DRIVER', 'mysql');
+// define('DB_HOST', 'localhost');
+// define('ROOT', 'http://localhost/school_management/');
+// define('ASSETS', 'http://localhost/school_management/assets/');
